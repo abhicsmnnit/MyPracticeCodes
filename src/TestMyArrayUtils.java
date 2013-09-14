e2878fa632f883e54ed9c7bd49e4a529539e26d6
Abhinav <abhicsmnnit@gmail.com>
2013-09-14 23:42:43 +0530

    More Tests for testing Binary Search and its versions

--- src/TestMyArrayUtils.java
+++ src/TestMyArrayUtils.java
@@ -1,3 +1,6 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
 /**
  * Created with IntelliJ IDEA.
  * User: abhinav
@@ -6,4 +9,47 @@
  * To change this template use File | Settings | File Templates.
  */
 public class TestMyArrayUtils {
+    @Test
+    public void bSearchShouldReturnPositiveValueForExistingElement() {
+        int[] array = {0, 1, 3, 4};
+        Assert.assertTrue(MyArrayUtils.bSearch(array, 0, array.length - 1, 3) > 0);
+    }
+
+    @Test
+    public void bSearchShouldReturnNegativeValueForNonExistingElement() {
+        int[] array = {0, 1, 3, 4};
+        Assert.assertTrue(MyArrayUtils.bSearch(array, 0, array.length - 1, 2) < 0);
+    }
+
+    @Test
+    public void bSearchFirstOccurrenceShouldReturnCorrectFirstOccurrenceForExistingElement() {
+        int[] array = {0, 1, 1, 3, 4};
+        Assert.assertEquals(MyArrayUtils.bSearchFirstOccurrence(array, 0, array.length, 1), 1);
+    }
+
+    @Test
+    public void bSearchFirstOccurrenceShouldReturnCorrectPositionForNonExistingElement() {
+        int[] array = {0, 1, 1, 3, 4};
+        Assert.assertEquals(MyArrayUtils.bSearchFirstOccurrence(array, 0, array.length, 2), -4);
+    }
+
+    @Test
+    public void bSearchLastOccurrenceShouldReturnCorrectLastOccurrenceForExistingElement() {
+        int[] array = {0, 1, 1, 1, 3, 5};
+
+        Assert.assertEquals(MyArrayUtils.bSearchLastOccurrence(array, 0, array.length, 1), 3);
+    }
+
+    @Test
+    public void bSearchLastOccurrenceShouldReturnCorrectPositionForNonExistingElement() {
+        int[] array = {0, 1, 1, 1, 3, 5};
+
+        Assert.assertEquals(MyArrayUtils.bSearchLastOccurrence(array, 0, array.length, 2), -5);
+    }
+
+    @Test
+    public void bSearchShouldCorrectPositionForNonExistingElement() {
+        int[] array = {0, 1, 3, 4};
+        Assert.assertEquals(MyArrayUtils.bSearch(array, 0, array.length - 1, 2), -3);
+    }
 }
