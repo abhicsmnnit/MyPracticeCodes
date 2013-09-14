e3bde5c7bf8aed037a035941df4e4ba94ac8718b
Abhinav <abhicsmnnit@gmail.com>
2013-09-14 23:39:35 +0530

    Made 3 versions of Binary Search and wrote tests for them: Normal Search, Search for first occurrence, Search for Last Occurrence

---
+++ src/MyArrayUtils.java
@@ -0,0 +1,56 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: abhinav
+ * Date: 14/9/13
+ * Time: 8:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyArrayUtils {
+    public static int bSearch(int[] array, int fromIndex, int toIndex, int key) {
+        return bSearchFirstOccurrence(array, fromIndex, toIndex, key);
+    }
+
+    public static int bSearchFirstOccurrence(int[] array, int fromIndex, int toIndex, int key) {
+        int low = fromIndex;
+        int high = toIndex;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (key < array[mid]) {
+                high = mid - 1;
+            }
+            else if (key > array[mid]) {
+                low = mid + 1;
+            }
+            else if (low < mid) {
+                high = mid;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -(mid + 1);
+    }
+
+    public static int bSearchLastOccurrence(int[] array, int fromIndex, int toIndex, int key) {
+        int low = fromIndex;
+        int high = toIndex;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (key < array[mid]) {
+                high = mid - 1;
+            }
+            else if (key > array[mid]) {
+                low = mid + 1;
+            }
+            else if (high > mid) {
+                low = mid;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -(mid + 1);
+    }
+}
---
+++ src/TestMyArrayUtils.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: abhinav
+ * Date: 14/9/13
+ * Time: 8:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestMyArrayUtils {
+}
